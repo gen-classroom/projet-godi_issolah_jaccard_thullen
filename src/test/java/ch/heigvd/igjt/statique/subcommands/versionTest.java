dd4186e0fec5b3f565d91854776505bb4bfb2c31
issolahma <22448610+issolahma@users.noreply.github.com>
2021-03-24 10:51:12 +0100

    Add test for subCommandVersion class

---
+++ src/test/java/ch/heigvd/igjt/statique/subcommands/versionTest.java
@@ -0,0 +1,20 @@
+package ch.heigvd.igjt.statique.subcommands;
+
+import org.junit.Test;
+
+import picocli.CommandLine;
+
+import static org.junit.Assert.assertEquals;
+
+public class versionTest {
+    //private Object SubCommandVersion;
+
+    @Test
+    public void shouldPrintCorrectVersion() throws Exception {
+        String expected = "0.0.1-SNAPSHOT";
+        SubCommandVersion params = CommandLine.populateCommand(new SubCommandVersion());
+        params.call();
+        String actual = params.version;
+        assertEquals(expected, actual);
+    }
+}
