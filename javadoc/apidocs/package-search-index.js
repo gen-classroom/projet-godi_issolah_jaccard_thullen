11982dbb0d23f81fdad76d258501886e5e3610de
CapitainMorgan <matthieugodi@vtx.ch>
2021-05-28 11:53:17 +0200

    Add javadoc

---
+++ javadoc/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ch.heigvd.igjt.statique.data"},{"l":"ch.heigvd.igjt.statique.modules"},{"l":"ch.heigvd.igjt.statique.subcommands"}];updateSearchResults();
